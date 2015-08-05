9c0d917e312c61451f4d093634879fa4516611b0
zhanglun <zhanglun1410@gmail.com>
2015-08-05 13:41:47 +0800

    update

--- test/test-app.js
+++ test/test-app.js
@@ -5,7 +5,7 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
 
-describe('aaa:app', function () {
+describe('generators:app', function () {
   before(function (done) {
 //    helpers.run(path.join(__dirname, '../generators/app'))
 //      .withOptions({ skipInstall: true })
