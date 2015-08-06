0c41942f8cdf207f133bcfd1d0447a9e1c60fdf1
zhanglun <zhanglun1410@gmail.com>
2015-08-06 17:29:31 +0800

    update package.json and test

--- test/test-app.js
+++ test/test-app.js
@@ -7,18 +7,17 @@ var os = require('os');
 
 describe('nodescaffold:app', function () {
   before(function (done) {
-//    helpers.run(path.join(__dirname, '../generators/app'))
-//      .withOptions({ skipInstall: true })
-  //    .withPrompts({ someOption: true })
-    //  .on('end', done);
+   helpers.run(path.join(__dirname, '../generators/app'))
+     .withOptions({ skipInstall: true })
+     .withPrompts({ someOption: true })
+     .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
       'bower.json',
       'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      '.editorconfig'
     ]);
   });
 });
