2740833830d8ecc38ff6eff995c36d786ac2389c
zhanglun <zhanglun1410@gmail.com>
2015-08-07 18:21:24 +0800

    remove test before()

--- test/test-app.js
+++ test/test-app.js
@@ -6,12 +6,12 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('nodescaffold:app', function () {
-  before(function (done) {
-   helpers.run(path.join(__dirname, '../generators/app'))
-     .withOptions({ skipInstall: true })
-     .withPrompts({ someOption: true })
-     .on('end', done);
-  });
+  // before(function (done) {
+  //  helpers.run(path.join(__dirname, '../generators/app'))
+  //    .withOptions({ skipInstall: true })
+  //    .withPrompts({ someOption: true })
+  //    .on('end', done);
+  // });
 
   it('creates files', function (done) {
     assert.file([
